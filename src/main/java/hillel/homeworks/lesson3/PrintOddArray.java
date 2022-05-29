762a19c20fa19a53ca0880d2154f999d29b55bbd
Konstantin Dovbish <dokongr@gmail.com>
2022-05-29 14:42:34 +0300

    PrintOddArray

---
+++ src/main/java/hillel/homeworks/lesson3/PrintOddArray.java
@@ -0,0 +1,11 @@
+package hillel.homeworks.lesson3;
+
+public class PrintOddArray {
+    public static void main(String[] args) {
+        System.out.println("Урок 3. ДЗ 8. Создайте массив, содержащий 10 первых нечетных чисел. Выведете элементы массива на консоль в одну строку, разделяя запятой.");
+        int[] a = {1, 3, 5, 7, 10, 13, 15, 17, 19, 21};
+        for (int i = 0; i <= a.length-1; i++) {
+            System.out.print(a[i] + ", ");
+        }
+    }
+}

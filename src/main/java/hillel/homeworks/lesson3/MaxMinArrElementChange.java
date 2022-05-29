aec5b4f8e301d322adf2434190954498626d4c7b
Konstantin Dovbish <dokongr@gmail.com>
2022-05-29 17:17:58 +0300

    MaxMinArrElementChange изменено 2

--- src/main/java/hillel/homeworks/lesson3/MaxMinArrElementChange.java
+++ src/main/java/hillel/homeworks/lesson3/MaxMinArrElementChange.java
@@ -34,39 +34,4 @@ public class MaxMinArrElementChange {
 
         System.out.println("Измененный массив: \n" + Arrays.toString(arr));
     }
-
-    /**
-     * Получить индекс минимального элемента массива
-     * @param arr Входной массив
-     * @return индекс минимального элемента
-     */
-    static int getMinElementIndex(int[] arr) {
-        int minIndex=0;
-        int minElement = arr[minIndex];
-        for (int i = 1; i <= arr.length-1; i++) {
-            if (arr[i] < minElement) {
-                minElement = arr[i];
-                minIndex = i;
-            }
-        }
-        return minIndex;
-    }
-
-
-    /**
-     * Получить индекс максимального элемента массивав
-     * @param arr Входной массив
-     * @return индекс максимального элемента
-     */
-    static int getMaxElementIndex(int[] arr) {
-        int maxIndex=0;
-        int maxElement=arr[maxIndex];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > maxElement) {
-                maxElement = arr[i];
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
 }

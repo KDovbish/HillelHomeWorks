3d32f16c900f07af61e47bdb3e2ac9ea37b8705c
Konstantin Dovbish <dokongr@gmail.com>
2022-07-03 09:49:03 +0300

    hw13. Убраны ненужные комментарии

--- src/test/java/hillel/homeworks/lesson13/intcollection/ServiceTest.java
+++ src/test/java/hillel/homeworks/lesson13/intcollection/ServiceTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServiceTest {
-
     @Test
     void calcIntCollAverage() {
         List<Integer> coll = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
